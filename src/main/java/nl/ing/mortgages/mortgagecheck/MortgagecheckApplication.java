9a7d7f007ad765fd9289dbf675209c4934a3a0a7
Diederik <maarten.diederik@sogeti.com>
2024-11-19 15:40:30 +0100

    init

---
+++ src/main/java/nl/ing/mortgages/mortgagecheck/MortgagecheckApplication.java
@@ -0,0 +1,13 @@
+package nl.ing.mortgages.mortgagecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgagecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgagecheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ing/mortgages/mortgagecheck/MortgagecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ing.mortgages.mortgagecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgagecheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
